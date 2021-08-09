7b6d50cc85ec9bb640c5f9d4735f39e1f5f61bbf
Kost Kikinov <kikinov.k@gmail.com>
2021-08-10 00:26:16 +0300

    feat: implement function that reverse digits of this number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+  // return Math.abs(n).toString(10).split('').reverse().join('')
+  return Math.abs(n).toString(10)
+                    .split('')
+                    .map((value, index, array) => array[array.length - index - 1])
+                    .join('')
 }
